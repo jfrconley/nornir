5f3fccd944eabb3b81563d6903ac563220d739b6
John Conley <john@blank.com>
2026-05-02 16:25:23 -0700

    exclude test package from coverage metrics

--- packages/test/jest.config.cjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  testRegex: "__tests__/dist/.+\\.spec\\.js$",
-  transform: {
-    '^.+\\.(ts)$': ['babel-jest', { rootMode: 'upward' }],
-  }
-};
